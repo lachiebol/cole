8f09e3a0da4969c4b7fe10d14b9b1e56e36ff930
lachiebol <64183081+lachiebol@users.noreply.github.com>
2024-10-24 20:53:47 +1300

    cole

--- main.js
+++ main.js
@@ -4,6 +4,41 @@ function randomNumber(max) {
     return Math.random() * max
 }
 
+
+
+function coleLikesCounterStrike(text) {
+    let words=text.split(' ');
+    for (let word of words) {
+
+        let content = document.getElementById('cole');
+
+
+        let wordDiv=document.createElement('div');
+        content.appendChild(wordDiv);
+        for(let letter of word) {
+            
+            let span=document.createElement('span');
+            span.textContent=letter;
+            span.style.transition='color 250ms linear';
+
+            wordDiv.appendChild(span);
+        }
+    }
+}
+
+coleLikesCounterStrike('Cole is fucking men');
+
+
+let rainbowColours=[
+    "#9400D3",
+    "#4B0082",
+    "#0000FF",
+    "#00FF00",
+    "#FFFF00",
+    "#FF7F00",
+    "#FF0000",
+]
+
 function cole() {
     let content = document.getElementById('cole');
 
@@ -11,6 +46,7 @@ function cole() {
 
     for (let span of spans) {
         span.classList.add('animation');
+
         if(span.classList.contains('rotated')) {
             content.style.transform=`rotate(${randomNumber(1)}turn)`;
             
@@ -22,6 +58,15 @@ function cole() {
             span.classList.add('rotated');
             content.style.left=`${randomNumber(window.innerWidth)}px`;
             content.style.top=`${randomNumber(window.innerHeight)}px`;
+        }
+
+
+        let letters=span.children;
+        for(let letter of letters) {
+
+            let newColourIndex=Math.floor(randomNumber(rainbowColours.length));
+
+            letter.style.color=`${rainbowColours[newColourIndex]}`;
         }
     }
 }
